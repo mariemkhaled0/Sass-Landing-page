2b6f6fa66bfb0552a3b0e55894c32651aefdb877
mariem <mariemkhaled834@gmail.com>
2025-09-04 23:44:59 +0300

    unInstall react-slidedown

--- src/components/FaqItem.jsx
+++ src/components/FaqItem.jsx
@@ -1,7 +1,6 @@
 import clsx from "clsx";
 import { useState } from "react";
-import { SlideDown } from "react-slidedown";
-import "react-slidedown/lib/slidedown.css";
+import SlideDown from "./SlideDown";
 
 const FaqItem = ({ item, index }) => {
   const [activeId, setActiveId] = useState(null);
@@ -41,7 +40,7 @@ const FaqItem = ({ item, index }) => {
         </div>
       </div>
 
-      <SlideDown>
+      <SlideDown isOpen={activeId === item.id}>
         {activeId === item.id && (
           <div className="body-3 px-7 py-3.5">{item.answer}</div>
         )}
